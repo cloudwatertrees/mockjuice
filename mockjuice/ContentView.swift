ceea10ca3bde8eb67d17e57c46916ad6966a8a4f
cloudwatertrees <cloudwatertrees@gmail.com>
2025-06-24 03:37:02 +0100

    Initial Commit

---
+++ mockjuice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mockjuice
+//
+//  Created by Noor Hassan on 24/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mockjuice/mockjuiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  mockjuiceApp.swift
+//  mockjuice
+//
+//  Created by Noor Hassan on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct mockjuiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
